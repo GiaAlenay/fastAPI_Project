aa74b6b25ab5f8827e83fde70e0725f88239228e
Gianella Leon <giigl@hotmail.com>
2023-05-10 14:56:32 -0500

    crud completo

--- create.db.py
+++ create.db.py
@@ -1,6 +1,5 @@
-from database import Base,engine
-from model import Item
+from main import Base,engine
+#para poder crear las tablas dentro de la db se necesita ejecutar este archivo :python create.db.py por si solo
+
 print('creating database ...')
 Base.metadata.create_all(engine)
-
-#para poder crear los cambios se necesita ejecutar este archivo :python create.db.py
