12d5c6e19e12fc9c2bbc34e1384f6a00b186b019
Gianella Leon <giigl@hotmail.com>
2023-05-10 23:17:12 -0500

    ruta puth con relcion de muchos a muchos

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker,relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,ForeignKey, Table
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional,List
 
 #cambiar con sus datos en este caso la base de datos la llame item_db
 DATABASE_URL = "postgresql://postgres:postgres@localhost/item_db"
@@ -14,27 +14,39 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 app = FastAPI()
+association_table = Table(
+    'user_item_association', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('item_id', Integer, ForeignKey('items.id'))
+)
 
 class Item(Base):
     __tablename__ = "items"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     description = Column(String)
+    users = relationship("User", secondary=association_table, back_populates="items")
+    # back_populates='items' no es por el nombre de la tabla si no por la referencia que tiene en la tabla de user
 
 class User(Base):
     __tablename__="users"
     id=Column(Integer, primary_key=True,index=True)
     full_name=Column(String(255),nullable=False )
     gender=Column(String(255), nullable=False )
+    items = relationship("Item", secondary=association_table, back_populates="users")
 
 
 class ItemAttributes(BaseModel):
+    id: Optional[int]
     name: Optional[str]
     description: Optional[str]
+    users: Optional[List[int]] 
 
 class UserAttributes(BaseModel):
+    id: Optional[int]
     full_name: Optional[str]
     gender: Optional[str]
+    items: Optional[List[int]] 
 
 def get_db():
     try:
@@ -67,11 +79,17 @@ def create_user(user: UserAttributes, db:Session=Depends(get_db)):
 def update_user(user_id:int , user: UserAttributes, db:Session=Depends(get_db)):
     db_user= db.query(User).filter(User.id==user_id).first()
     if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found.")
     if user.full_name:
         db_user.full_name=user.full_name
     if user.gender:
         db_user.gender=user.gender
+    if user.items:
+       items = db.query(Item).filter(Item.id.in_(user.items)).all()
+       if len(items) != len(user.items):
+           raise HTTPException(status_code=404, detail="One or more items not found.")
+       db_user.items = items
+        
     db.commit()
     db.refresh(db_user)
     return db_user
@@ -84,8 +102,8 @@ def delete_user(user_id:int, db:Session=Depends(get_db)):
     db.delete(user)
     db.commit()
     return user    
+@app.put("/users/{user_id}")
 
-@app.get("/items/")
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = db.query(Item).offset(skip).limit(limit).all()
     return items
